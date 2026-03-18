c3bf01ce23f569a93cf8be99ae8e96b833855796
goldic <contact@goldic.xyz>
2026-03-18 18:04:41 +0400

    Fix: update and downgrade deps to fix build

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,8 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "react-native-reanimated/plugin",
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-worklets/plugin',
       [
         "module-resolver",
         {
